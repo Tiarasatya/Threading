d6e375f10f7334b7e9b8bd615f5f0ff6bb4f3c21
Tiarasatya <82933768+Tiarasatya@users.noreply.github.com>
2022-03-26 00:32:29 +0700

    Update Source.cpp

--- Threading/Source.cpp
+++ Threading/Source.cpp
@@ -3,30 +3,42 @@
 
 using namespace std;
 
-void print()
+//function thread untuk print odd,even number
+//parameter limit sebagai batasan angka yang akan dilooping
+void printNumber(int limit)
 {
-	for (int i = 1; i <= 20; i++)
+	for (int i = 0; i <= limit; i++)
 	{
+		//Jika angka ganjil, print odd number 
 		if (i % 2 != 0)
 		{
-			cout << i << "odd" << endl;
+			//print angka ganjil (angka+odd)
+			cout << i << "  odd" << endl;
+			//untuk delay 1 second setelah angka ganjil di print
+			//1 second = 1000 milliseconds
 			this_thread::sleep_for(chrono::milliseconds(1000));
 		}
+		//Jika angka genap, print even number
 		else if(i % 2 == 0)
 		{
-			cout << i << "even" << endl;
+			//print angka genap (angka+even)
+			cout << i << "  even" << endl;
+			//untuk delay 2.5 second setelah angka genap di print
+			//1 second = 1000 milliseconds, sehingga 2.5 second = 2500 millisecond
 			this_thread::sleep_for(chrono::milliseconds(2500));
 		}
 	}
-	cout << "finished" << endl;
+	//looping selesai (telah mencapai limit)
+	cout << "finished" << endl; 
 }
 
 int main()
 {
-	thread th(print);
-	
-	th.join();
-	
-	return 0;
-}
+	//banyaknya angka yang akan dilooping
+	int limit = 25;
 
+	//memulai thread
+	thread th(printNumber,(limit));
+	//menunggu thread selesai
+	th.join();
+}
